3ad7bae3954132b86f129d80a23df9792ace4c3e
Israel dela Cruz <igdcruz42@gmail.com>
2019-06-13 01:18:07 -0500

    second

--- zoos/src/main/kotlin/com/isra/zoos/service/ZooService.kt
+++ zoos/src/main/kotlin/com/isra/zoos/service/ZooService.kt
@@ -5,5 +5,8 @@ import com.isra.zoos.model.Zoo
 interface ZooService {
 
     fun findAll(): MutableList<Zoo>
+    fun update(zoo: Zoo, id: Long): Zoo
+    fun save(zoo: Zoo): Zoo
+    fun delete(id: Long)
 
 }
--- zoos/src/main/kotlin/com/isra/zoos/service/ZooServiceImpl.kt
+++ zoos/src/main/kotlin/com/isra/zoos/service/ZooServiceImpl.kt
@@ -5,6 +5,8 @@ import com.isra.zoos.repository.ZooRepository
 import com.isra.zoos.views.CountAnimalsInZoos
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import javax.persistence.EntityNotFoundException
 
 @Service
 class ZooServiceImpl : ZooService {
@@ -16,6 +18,33 @@ class ZooServiceImpl : ZooService {
         val outZoos = mutableListOf<Zoo>()
         zooRepository.findAll().toCollection(outZoos)
         return outZoos
+    }
+
+    @Transactional
+    override fun update(zoo: Zoo, id: Long): Zoo {
+        val currentZoo = zooRepository.findById(id)
+                .orElseThrow { EntityNotFoundException("Zoo id $id cannot be found!") }
+
+        if (zoo.zooname != null) currentZoo.zooname = zoo.zooname
+
+        return zooRepository.save(currentZoo)
+    }
+
+    override fun save(zoo: Zoo): Zoo {
+        val newZoo = Zoo(zoo.zooname)
+
+        return zooRepository.save(newZoo)
+    }
+
+    @Transactional
+    override fun delete(id: Long) {
+        if (zooRepository.existsById(id)) {
+            zooRepository.deleteZooFromZooanimals(id)
+            zooRepository.deleteById(id)
+        } else {
+            throw EntityNotFoundException("Zoo id $id cannot be found!")
+        }
+
     }
 
 }
